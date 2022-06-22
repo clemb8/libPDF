c0f764026c090bd185e78e5f81b98e0f07954c86
clemlifen <clement.boschet@lifen.fr>
2022-06-22 18:07:37 +0200

    Fix arguments

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const PDFMerger = require('pdf-merger-js');
 var merger = new PDFMerger();
 
 (async () => {
-    merger.add(process.argv[0], [1]); 
-    merger.add(process.argv[1], [2]);
+    console.log(process.argv[2]);
+    merger.add(process.argv[2], [1]); 
+    merger.add(process.argv[3], [2]);
 
     await merger.save('merged.pdf');
 })();
